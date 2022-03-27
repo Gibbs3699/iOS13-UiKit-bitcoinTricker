4bdbc20f1af45c5d4339c00805d6027ef00c15cb
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-03-27 23:29:08 +0700

    Doing network call and update unlabeled

--- iOS13-UiKit-bitcoinTricker/Controller/ViewController.swift
+++ iOS13-UiKit-bitcoinTricker/Controller/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var coinManager = CoinManager()
+    
     @IBOutlet weak var cotentView: UIView!
     @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -17,9 +19,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         cotentView.layer.cornerRadius = 10
+        
+        coinManager.delegate = self
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
     }
 
 
+}
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectCurrency)
+    }
+}
+
+extension ViewController: CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+    
+    
 }
 
---
+++ iOS13-UiKit-bitcoinTricker/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  iOS13-UiKit-bitcoinTricker
+//
+//  Created by TheGIZzz on 27/3/2565 BE.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
